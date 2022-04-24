ac46aca5431163b558c0c13de61a7e3d07d22555
AbidSaleem <abidsaleem@live.com>
2022-04-25 00:51:09 +0300

    Finish the play button and the information bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Algorithms } from './core/enums/algorithms-enums';
-import {NodeModel} from './core/models/node.model';
+import { NodeModel } from './core/models/node.model';
 
 
 @Component({
@@ -14,15 +14,13 @@ export class AppComponent implements OnInit {
   algorithm = Algorithms.NONE;
 
   constructor() {
-    for(let i = 0; i < 32; i++) {
-      let row:NodeModel[] = [];
-      for(let j = 0; j < 70; j++) {
+    for (let i = 0; i < 30; i++) {
+      let row: NodeModel[] = [];
+      for (let j = 0; j < 73; j++) {
         row.push(new NodeModel(false, false, false, false, i, j));
       }
       this.grids.push(row);
     }
-
-    console.log(this.grids);
   }
 
   ngOnInit(): void {
@@ -34,7 +32,13 @@ export class AppComponent implements OnInit {
   }
 
   algorithmSelected(algorthm: string) {
-
+    switch (algorthm) {
+      case 'Dijkstra':
+        this.algorithm = Algorithms.DIJKSTRA;
+        break;
+      case 'A*':
+        this.algorithm = Algorithms.A_STAR;
+    }
   }
 
   playClicked() {
--- src/app/core/enums/algorithms-enums.ts
+++ src/app/core/enums/algorithms-enums.ts
@@ -1,8 +1,5 @@
 export enum Algorithms {
   NONE = "",
-  BUBBLE_SORT = "Bubble Sort",
-  SELECTION_SORT = "Selection Sort",
-  INSERTION_SORT = "Insertion Sort",
-  MERGE_SORT = "Merge Sort",
-  QUICK_SORT = "Quick Sort"
+  DIJKSTRA = "Dijkstra",
+  A_STAR = "A*"
 }
